6fb80044cd751e781499ad8037000c80dfb2dc07
AlbertVeli <albert.veli@gmail.com>
2020-01-22 12:44:43 +0100

    Add route_to_duts

---
+++ bin/route_to_duts.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+dunkelip=`host dunkelstern.westermo.com | cut -d' ' -f4`
+
+# Need sudo
+if test "`uname -s`" = "Darwin"; then
+    sudo route -n add -net 198.18.0.0/16 $dunkelip
+else
+    ip route add 198.18.0.0/16 via $dunkelip
+fi
